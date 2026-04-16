a8700114ef3c9a5293577d4c43e2fbde57b706f7
Sheila Maria Morales Baron + 12/02/2026 <smmb37@git.es>
2026-04-16 16:47:27 +0200

    Corrección de LOS TEST FINAL

--- src/main/java/org/lapaloma/hogwarts/service/CasaService.java
+++ src/main/java/org/lapaloma/hogwarts/service/CasaService.java
@@ -45,7 +45,7 @@ public class CasaService {
         List<Casa> lista = casaDAO.obtenerListaCasas();
 
         // Esto provoca error
-        lista = null;
+        //lista = null;
         
         if (lista == null || lista.isEmpty()) {
             throw new RuntimeException("No hay casas disponibles");
